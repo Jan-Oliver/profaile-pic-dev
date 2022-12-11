02a38a57fd5f4bb61b004b1c0cf1f9057cb368b7
Jan-Oliver Seidenfuss <jan-oliver@seidenfuss.com>
2022-12-11 11:39:20 +0100

    add dreambooth script for SD2.x

---
+++ scripts/setup_dreambooth_env.sh
@@ -0,0 +1,8 @@
+pip install -qq git+https://github.com/Jan-Oliver/diffusers
+pip install -q -U --pre triton
+pip install -q accelerate==0.12.0 transformers ftfy bitsandbytes gradio natsort
+pip install -q wandb
+# Install xformers wheel
+pip install -q https://github.com/brian6091/xformers-wheels/releases/download/0.0.15.dev0%2B4c06c79/xformers-0.0.15.dev0+4c06c79.d20221205-cp38-cp38-linux_x86_64.whl
+
+
